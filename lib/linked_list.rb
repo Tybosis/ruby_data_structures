923d5548b160ea5a258c3419b55e1cb3290f1cf3
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-06 16:13:48 -0700

    linked list implementation all tests passing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -13,23 +13,62 @@
 
 # to_s will print a comma-separated list of all values in the all nodes,
 # whatever those values might happen to be: "12, 'sam', :item, 'tango', 42"
-
+require 'pry'
 
 class LinkedList
-  def initialize
+  attr_accessor :head
+
+  def initialize(input_value = nil)
+    @head = Node.new(input_value, nil)
   end
 
-  def insert(node)
+  def insert(input_value)
+    current = @head
+    @head = Node.new(input_value, current)
   end
 
-  def search(val)
+  def search(search_terms)
+    current = @head
+    until current.next_node.nil?
+      return current if current.value == search_terms
+      current = current.next_node
+    end
+    nil
   end
 
-  def remove(node)
+  def remove(input_value)
+    current = @head
+    if current.value == input_value
+      @head = (current.next_node.nil? ? Node.new(nil, nil) : current.next_node)
+      return current
+    end
+    until current.next_node.nil?
+      previous = current
+      current = current.next_node
+      if current.value == input_value
+        previous.next_node = current.next_node
+        current.next_node = nil
+        current
+      end
+    end
   end
 
   def to_s
+    current = @head
     ans = ''
-    ans += Node.val until Node.val = nil
+    until current.next_node.nil?
+      ans += "#{current.value}, "
+      current = current.next_node
+    end
+    ans += "#{current.value}"
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
   end
 end
