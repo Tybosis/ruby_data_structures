d19cfeefce20001a3b96029e2ac5d50a8cea2422
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-15 07:23:37 -0700

    add benchmarking for hashtable

---
+++ lib/hash_benchmark.rb
@@ -0,0 +1,27 @@
+require 'benchmark'
+require_relative 'hashtable'
+require_relative 'hash_linked_list'
+
+words = []
+File.open('/usr/share/dict/words').each_line { |word| words << word }
+hash1 = Hashtable.new(1_000, HashLinkedList)
+hash2 = Hashtable.new(125_000, HashLinkedList)
+
+Benchmark.bm do |x|
+
+  x.report("Hashtable1 set") do
+    words.each { |word|  hash1.set(word, word.reverse) }
+  end
+
+  x.report("Hashtable1 get") do
+    words.each { |word| hash1.get(word) }
+  end
+
+  x.report("Hashtable2 set") do
+    words.each { |word|  hash2.set(word, word.reverse) }
+  end
+
+  x.report("Hashtable2 get") do
+    words.each { |word| hash2.get(word) }
+  end
+end
