a1658341edae307bfffecf1d161f1a92cda6c79c
Tyler Pottle <tyler.pottle@gmail.com>
2015-05-12 15:38:04 -0700

    double linked list working

--- lib/doubly_linked_list.rb
+++ lib/doubly_linked_list.rb
@@ -2,14 +2,14 @@ require 'linked_node'
 class DoubleLinkedList
   attr_reader :head, :tail
 
-  def initialize(head, node)
-    @tail, @head = node.new (head, nil)
+  def initialize(value, node)
+    @head = @tail = node.new(value, nil)
   end
 
   def insert(value, node)
-    if @tail.nil?
-      @head.nil? ? @tail, @head = node.new(value, nil) : @head = node.new(value, @head)
-    elsif @head.nil?
+    if @head.nil?
+      @head = @tail = node.new(value, nil)
+    elsif @head == @tail
       @head = node.new(value, @tail)
     else
       current = @head
@@ -19,19 +19,41 @@ class DoubleLinkedList
 
   def remove(input_value)
     current = @head
-    return "list is already empty!" if @head.nil? && @tail.nil?
-    if @head.value == input_value && @tail.nil?
-      @head = nil
-      return current
+    old_tail = @tail
+    return "list is already empty!" if @head.nil?
+    if @head.value == input_value
+      if @head == @tail
+        @head = @tail = nil
+        return
+      else
+        @head = @head.next_node
+        return current
+      end
     end
+    if (@head.value == input_value || @tail.value == input_value) && @head.next_node == @tail
+      if @head.value == input_value
+        @head.next_node = nil
+        @head = @tail
+        return current
+      else
+        @head.next_node = nil
+        @tail = @head
+        return old_tail
+      end
     end
     until current.next_node.nil?
       previous = current
       current = current.next_node
       if current.value == input_value
-        previous.next_node = current.next_node
-        current.next_node = nil
-        current
+        if current == @tail
+          @tail = previous
+          @tail.next_node = nil
+          return
+        else
+          previous.next_node = current.next_node
+          current.next_node = nil
+          current
+        end
       end
     end
   end
